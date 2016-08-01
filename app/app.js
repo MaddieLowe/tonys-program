c054f8929a8263a598a9f643c5a2e9441a340542
Madison Lowe <madison@morpheusmedical.net>
2016-08-01 17:49:13 -0600

    Change the model when the user selects cards

--- app/app.js
+++ app/app.js
@@ -1,6 +1,19 @@
 var range_selector = require('./widgets/range-selector');
+var base_range = require('./lib/base-range');
 
 $(document).ready(function() {
     var element = $('.range');
-    range_selector(element);
+    var model = JSON.parse(JSON.stringify(base_range));
+    var rs_emitter = range_selector(element, model);
+    rs_emitter.on('changed', function() {
+        var message = "Selected cards: ";
+        for (var i = 0; i < model.length; i++) {
+            for (var j = 0; j < model[i].length; j++) {
+                if (model[i][j].selected) {
+                    message += " " + model[i][j].name;
+                }
+            }
+        }
+        console.error(message);
+    });
 });
--- app/widgets/range-selector.js
+++ app/widgets/range-selector.js
@@ -1,28 +1,35 @@
-var base_range = require('../lib/base-range');
+var EventEmitter = require('events');
 
-module.exports = function(element) {
-    var click_handler = function(column) {
+module.exports = function(element, model) {
+    var emitter = new EventEmitter();
+
+    var click_handler = function(column, item) {
         return function(event) {
             if (column.hasClass('selected')) {
                 column.removeClass('selected');
+                item.selected = false;
             } else {
                 column.addClass('selected');
+                item.selected = true;
             }
+            emitter.emit('changed');
         };
     };
 
     // Build the range grid
-    for (var i = 0; i < base_range.length; i++) {
-        var row_range = base_range[i];
+    for (var i = 0; i < model.length; i++) {
+        var row_range = model[i];
         var row = $("<div class='row'></div>");
         for (var j = 0; j < row_range.length; j++) {
             var column = $("<div class='column'>" + row_range[j].name + "</div>");
 
             // TODO: teardown
-            column.on('click', click_handler(column));
+            column.on('click', click_handler(column, row_range[j]));
 
             row.append(column);
         }
         element.append(row);
     }
+
+    return emitter;
 };
