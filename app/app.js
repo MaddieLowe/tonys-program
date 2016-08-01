930574cfb0c081612354261dc2a2605767f279bb
Madison Lowe <madison@morpheusmedical.net>
2016-07-31 23:29:17 -0600

    Dynamically generate a poker range DOM element based on an input array

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
-console.log('Here\'s some stuff');
+var base_range = require('./lib/base-range');
+
+$(document).ready(function() {
+    var element = $('.range');
+
+    for (var i = 0; i < base_range.length; i++) {
+        var row_range = base_range[i];
+        var row = $("<div class='row'></div>");
+        for (var j = 0; j < row_range.length; j++) {
+            var column = $("<div class='column'>" + row_range[j].name + "</div>");
+            row.append(column);
+        }
+        element.append(row);
+    }
+});
---
+++ app/lib/base-range.js
@@ -0,0 +1,81 @@
+module.exports = [
+    [
+        {
+            name: "AA"
+        },
+        {
+            name: "AKs"
+        },
+        {
+            name: "AQs"
+        },
+        {
+            name: "AJs"
+        },
+        {
+            name: "ATs"
+        },
+        {
+            name: "A9s"
+        },
+        {
+            name: "A8s"
+        },
+        {
+            name: "A7s"
+        },
+        {
+            name: "A6s"
+        },
+        {
+            name: "A5s"
+        },
+        {
+            name: "A4s"
+        },
+        {
+            name: "A3s"
+        },
+        {
+            name: "A2s"
+        }
+    ],
+    [
+        {
+            name: "AKo"
+        },
+        {
+            name: "KK"
+        },
+        {
+            name: "KQs"
+        },
+        {
+            name: "KJs"
+        },
+        {
+            name: "K9s"
+        },
+        {
+            name: "K8s"
+        },
+        {
+            name: "K7s"
+        },
+        {
+            name: "K6s"
+        },
+        {
+            name: "K5s"
+        },
+        {
+            name: "K4s"
+        },
+        {
+            name: "K3s"
+        },
+        {
+            name: "K2s"
+        }
+    ]
+];
