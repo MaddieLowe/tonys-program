e5afc83b7c9f321fa2c76d86ebc77031203411a9
Madison Lowe <madisonrlowe@gmail.com>
2017-03-12 12:49:21 -0600

    More situation functionality and styling

--- app/lib/card-utils.js
+++ app/lib/card-utils.js
@@ -27,7 +27,7 @@ var card_from_value = module.exports.card_from_value = function(val) {
     return str;
 };
 
-module.exports.get_random_card = function(invalid_cards) {
+module.exports.get_random_card = function(invalid_cards, valid_cards) {
     var value;
     var suit;
 
@@ -35,18 +35,45 @@ module.exports.get_random_card = function(invalid_cards) {
         return c.value === value & c.suit === suit;
     };
 
+    var get_card_from_deck = function() {
+        var v = Math.round(Math.random() * (14 - 2) + 2);
+        var s = Math.round(Math.random() * 3);
+        value = card_from_value(v);
+        suit = suit_map[s];
+    };
+
+    var get_card_from_range = function() {
+        var i = Math.round(Math.random() * ((valid_cards.length - 1) - 0) + 0);
+        console.error("valid cards length is " + valid_cards.length + " i is " + i);
+        value = card_from_value(valid_cards[i].value);
+        suit = valid_cards[i].suit;
+    };
+
     var count = 0;
     do {
         if (count !== 0) {
             console.log("Re-getting card because we already have " + value + suit);
         }
+        if (count === 10) {
+            break;
+        }
         count++;
 
-        var v = Math.round(Math.random() * (14 - 2) + 2);
-        var s = Math.round(Math.random() * 3);
-        value = card_from_value(v);
-        suit = suit_map[s];
+        if (valid_cards) {
+            get_card_from_range();
+        } else {
+            get_card_from_deck();
+        }
     } while (invalid_cards.find(card_matches))
 
     return new card(value + suit);
+};
+
+module.exports.pairs_array_to_card_array = function(pairs_array) {
+    var cards = [];
+    for (var i = 0; i < pairs_array.length; i++) {
+        cards.push(new card(pairs_array[i].card1.name));
+        cards.push(new card(pairs_array[i].card2.name));
+    }
+    return cards;
 };
--- app/lib/situation.js
+++ app/lib/situation.js
@@ -1,10 +1,16 @@
 var get_random_card = require('./card-utils').get_random_card;
+var pairs_array_to_card_array = require('./card-utils').pairs_array_to_card_array;
 
 function situation(config) {
     let board_size = config.board_size || 3;
     this.type = config.type;
     this.position = config.position;
+    this.positions = config.positions;
     this.pot = config.pot;
+
+    if (config.hand_range) {
+        this.hand_range = pairs_array_to_card_array(JSON.parse(config.hand_range));
+    }
 
     this.board = [];
     for (let i = 0; i < board_size; i++) {
@@ -13,7 +19,7 @@ function situation(config) {
 
     this.hand = [];
     for (let i = 0; i < 2; i++) {
-        this.hand[i] = get_random_card(this.board.concat(this.hand));
+        this.hand[i] = get_random_card(this.board.concat(this.hand), this.hand_range);
     }
 }
 
