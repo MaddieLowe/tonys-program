67a61fb4a9123291f1a62e211aac481d21a12ad1
Madison Lowe <madisonrlowe@gmail.com>
2017-03-12 21:36:54 -0600

    Remove save button

---
+++ app/range-analysis.js
@@ -0,0 +1,54 @@
+var poker_range_selector = require('./widgets/poker-range-selector');
+var card_selector = require('./widgets/card-selector');
+var rank_hands = require('./lib/rank-hands');
+var rank_filter = require('./lib/filters/rank-filter');
+
+$(document).ready(function() {
+    var poker_range_element = $('poker-range');
+    var selected_range = [];
+    var poker_range_emitter = poker_range_selector(poker_range_element, selected_range);
+
+    var card_selector_el = $('card-selector');
+    var selected_cards = [];
+    var card_selector_emitter = card_selector(card_selector_el, selected_cards);
+
+    var combine_selected_cards = function() {
+        var final_range = [];
+        for (var i = 0; i < selected_range.length; i++) {
+            if (!selected_cards.find(function(element) {
+                return selected_range[i].contains(element);
+            })) {
+                final_range.push(selected_range[i]);
+            }
+        }
+        // var message = "Selected cards:";
+        // for (var i = 0; i < final_range.length; i++) {
+        //     message += " " + final_range[i].name;
+        // }
+        // console.error(message);
+
+        var output = "";
+        output +="<div>Combinations in range: " + final_range.length + "</div><div>&nbsp;</div>";
+        if (selected_cards.length >= 3) {
+            var combos = rank_hands.rank_table(final_range, selected_cards);
+            rank_filter(combos, function(type, value) {
+            // for (var type in combos) {
+                output += "<div>" + type + ": " + value + "</div>";
+                // console.error(type + ": " + value);
+            });
+        }
+        var output_div = $('.output');
+        output_div.empty();
+        output_div.append(output);
+
+        return combos;
+    };
+
+    poker_range_emitter.on('changed', function() {
+        combine_selected_cards();
+    });
+
+    card_selector_emitter.on('changed', function() {
+        combine_selected_cards();
+    });
+});
---
+++ app/situation-drills.js
@@ -0,0 +1,91 @@
+var situations = require('./lib/situations/situations');
+var situation = require('./lib/situation');
+var situation_display = require('./widgets/situation-display');
+var drop_down_menu = require('./widgets/drop-down-menu');
+var get_random_position = require('./lib/card-utils').get_random_position;
+
+$(document).ready(function() {
+    var situation_el = $('drop-down-menu.situation');
+    var situations_emitter = new drop_down_menu(situation_el, Object.keys(situations));
+    var current_situation_type = situations[Object.keys(situations)[0]];
+    situations_emitter.on('change', function(selected) {
+        current_situation_type = selected;
+    });
+
+    var completed_hands = [];
+    var update_count = function() {
+        $('.hands-completed').text(completed_hands.length);
+    };
+    update_count();
+
+    var position_random = true;
+    var position;
+    var setup_positions_dropdown = function() {
+        var positions = ['Random'].concat(current_situation.positions);
+        var positions_el = $('drop-down-menu.position');
+        var positions_emitter = new drop_down_menu(positions_el, positions);
+        position_random = true;
+
+        positions_emitter.on('change', function(selected) {
+            if (selected === 'Random') {
+                position_random = true;
+                position = get_random_position(current_situation.positions);
+            } else {
+                position_random = false;
+                position = selected;
+            }
+            current_situation.set_position(position);
+            update_situation_display(current_situation);
+        });
+    };
+
+    var current_situation;
+    var get_new_situation = function() {
+        current_situation = new situation(current_situation_type);
+        if (!position_random) {
+            current_situation.set_position(position);
+        }
+        update_situation_display(current_situation);
+        update_count();
+    };
+
+    var update_situation_display = function(situation) {
+        var situation_display_el = $('situation-display');
+        situation_display_el.children().remove();
+        var situation_display_emitter = new situation_display(situation_display_el, situation);
+        situation_display_emitter.on('change', function() {
+            if (situation.action) {
+                completed_hands.push(current_situation);
+                get_new_situation();
+            }
+        });
+    };
+
+    var new_session_el = $('.new-session');
+    new_session_el.on('click', function() {
+        completed_hands = [];
+        get_new_situation();
+        setup_positions_dropdown();
+    });
+
+    get_new_situation();
+    setup_positions_dropdown();
+
+    var download_el = $('.download');
+    var a_el = document.createElement('a');
+    document.body.appendChild(a_el);
+    a_el.style = "display: none";
+    download_el.on('click', function() {
+        var text_file = "";
+        completed_hands.forEach(function(hand) {
+            text_file = text_file + hand.create_export_data() + "\n\n\n\n";
+        });
+
+        var blob = new Blob([text_file], {type: 'text/plain'});
+        var url = window.URL.createObjectURL(blob);
+        a_el.href = url;
+        a_el.download = 'session.txt';
+        a_el.click();
+        window.URL.revokeObjectURL(url);
+    });
+});
