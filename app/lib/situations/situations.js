91ac41f7225377d172d5ee3c41d82fbf8469a83e
LtGrimms <alcridla@gmail.com>
2017-05-06 20:34:08 -0600

    changed name of SB3betCbet situation

--- app/lib/situations/situations.js
+++ app/lib/situations/situations.js
@@ -1,6 +1,6 @@
 let situations = {
     "Continuation Betting vs Big Blind": require('./continuation-betting-vs-bb'),
-    "Continuation Betting as SB after 3bet": require('./continuation-bet-from-small-blind-3bet-pot')
+    "Continuation Betting SB 3bet": require('./continuation-bet-from-small-blind-3bet-pot')
 };
 
 module.exports = situations;
