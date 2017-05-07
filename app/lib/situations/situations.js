33ecdb645dc277bb60555c6a82417fff94ab2239
LtGrimms <alcridla@gmail.com>
2017-05-06 19:34:25 -0600

    began adding SB3bet situation

--- app/lib/situations/situations.js
+++ app/lib/situations/situations.js
@@ -1,5 +1,6 @@
 let situations = {
-    "Continuation Betting vs Big Blind": require('./continuation-betting-vs-bb')
+    "Continuation Betting vs Big Blind": require('./continuation-betting-vs-bb'),
+    "Continuation Betting as SB after 3bet": require('./continuation-bet-from-small-blind-3bet-pot')
 };
 
 module.exports = situations;
